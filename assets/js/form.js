d2e68d208c43888ca1000c208bb3dc832043b420
midhun k paniker <91378594+midhunkalarikkal@users.noreply.github.com>
2023-09-25 01:42:22 +0530

    first push

---
+++ assets/js/form.js
@@ -0,0 +1,16 @@
+$("#submit-form").submit((e)=>{
+    e.preventDefault()
+    $.ajax({
+        url:"https://script.google.com/macros/s/AKfycbyK2zfAWz7bLdhyvxdc3XkOLxD6LrtdiyShauW9tWRI78_mcPdHuIU0MnzMG0Hr0eMx/exec",
+        data:$("#submit-form").serialize(),
+        method:"post",
+        success:function (response){
+            alert("Form submitted successfully")
+            // window.location.reload()
+            window.location.href="index.html"
+        },
+        error:function (err){
+            alert("Something Error")
+        }
+    })
+})
